233bf658bf3fca807047f5f39e2007167e84be5c
酒井 悠 <sakai-yuu-1203@docomo.ne.jp>
2019-09-12 13:30:38 +0900

    add.基本クラスの追加

---
+++ LoopShoot/LS/Def/Screen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LS.Def
+{
+    static class Screen
+    {
+        // 画面の大きさをここで指定
+        public static readonly int Width = 800;//画面横幅
+        public static readonly int Height = 600;//画面縦幅
+    }
+}
